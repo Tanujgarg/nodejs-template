49f14cb53da00884125969575da858098146ad6c
Tanuj Garg <tanujgarg@y7mail.com>
2020-01-27 10:45:49 +0530

    Dependencies
    fixes

--- common-dependencies.sh
+++ common-dependencies.sh
@@ -1,7 +1,7 @@
 npm install express mongoose cors body-parser dotenv --save
 
 npm install \
-  typescript ts-node \
+  typescript ts-node nodemon \
   swagger-jsdoc swagger-ui-express tslint \
   jest mocha chai @types/node @types/express \
   @types/mongoose @types/cors @types/body-parser \
