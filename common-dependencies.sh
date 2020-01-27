4a654d56fdaeea5616ae8701c53e5d2d1b169034
Tanuj Garg <tanujgarg@y7mail.com>
2020-01-27 10:43:00 +0530

    Dependencies
    types added

--- common-dependencies.sh
+++ common-dependencies.sh
@@ -4,5 +4,5 @@ npm install \
   typescript ts-node \
   swagger-jsdoc swagger-ui-express tslint \
   jest mocha chai @types/node @types/express \
-  @types/mongoose @types/cors \
+  @types/mongoose @types/cors @types/body-parser \
   --save-dev
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
-// import cors from "cors";
+import cors from 'cors';
 
 const app = express();
 
-// app.use(cors({origin: true}));
+app.use( cors( {
+  origin: true,
+  credentials: true
+} ) );
 app.use( bodyParser.json() );
 
 
